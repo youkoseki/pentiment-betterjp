aefed8882660bbc32ee661f90deb542713e449f5
Yu Koseki <2663424+youkoseki@users.noreply.github.com>
2023-12-26 18:16:16 +0900

    bug fix

--- tools/pentiment_tsv2json.py
+++ tools/pentiment_tsv2json.py
@@ -11,7 +11,7 @@ p1 = re.compile('^\'')
 p2 = re.compile('\'$')
 
 df = pd.read_table('../output/Pentiment-BetterJP - out.tsv',usecols=[0,1,2,4,5,6,7])
-df = df.query('BetterJP.notnull() or Duplicate.notnull()')
+df = df.query('BetterJP.notnull() or Duplicate.notnull() or AutoUpdate.notnull()')
 
 dj = {}
 dj["StringTables"]  = []
