f1de89f6cdffe6ad72f3af29d8e21c4418cd7857
Yu Koseki <2663424+youkoseki@users.noreply.github.com>
2023-12-30 22:44:56 +0900

    updated translations

--- tools/9-machinechecker..py
+++ tools/9-machinechecker..py
@@ -1,12 +1,18 @@
 # coding:utf-8
 import os
 
+duplicate = {}
 with open("dictionary.tsv") as f:
 	for s_line in f:
 		s_line = s_line.rstrip()
 		keys = s_line.split("\t")
+		if keys[0] in duplicate:
+			print("DUPLICATE- "+keys[1])
+			continue
+		duplicate[keys[0]] = keys[1]
+
 		print("# "+keys[1])
 		rt = os.system('grep "'+keys[0]+'" "../output/Pentiment-BetterJP - out.tsv" | grep -v '+keys[1]) >> 8
 		print(rt)
-
+		print()
 
