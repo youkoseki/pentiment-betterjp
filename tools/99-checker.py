2abc08100644fe9f9ad57a4394f5b31819176445
Yu Koseki <2663424+youkoseki@users.noreply.github.com>
2023-12-31 09:35:22 +0900

    updated translations

--- tools/99-checker.py
+++ tools/99-checker.py
@@ -23,35 +23,35 @@ for index, row in da.iterrows():
 
 n = 0
 
-print("## changes")
+print("# changes")
 
 for index, row in df.iterrows():	
 	if row["N"] in machine_translation:#機械翻訳がある
 #		print("AA")
 		if (pd.notnull(row["BetterJP"])): #手動訳もある
 			if( machine_translation[row["N"]] != row["BetterJP"]): #両者が違う
-				print("#"+str(row["N"]))
+				print("## "+str(row["N"]))
 				print(machine_translation[row["N"]])
 				print(row["BetterJP"])
 
 print()
-print("## same w/machine translation")
+print("# same w/machine translation")
 
 for index, row in df.iterrows():	
 	if (row["N"]) in machine_translation:#機械翻訳がある
 		if (pd.notnull(row["BetterJP"])): #手動訳もある
 			if( machine_translation[row["N"]] == row["BetterJP"]): #両者が同じ
-				print("#"+str(row["N"]))
+				print("## "+str(row["N"]))
 				print(row["BetterJP"])
 
 
 print()
-print("## same w/original")
+print("# same w/original")
 
 for index, row in df.iterrows():	
 	if (pd.notnull(row["BetterJP"])): #手動訳がある
 		if(row["Japanese"] == row["BetterJP"]): #もとと同じ
-			print("#"+str(row["N"]))
+			print("## "+str(row["N"]))
 			print(row["BetterJP"])
 
 
@@ -62,12 +62,14 @@ for index, row in dc.iterrows():
 	if (pd.notnull(row["English"]) and pd.notnull(row["Japanese"])):
 		dictionary[row["English"]] = row["Japanese"]
 
-dict_keys = dictionary.keys()
+dict_keys = list(dictionary.keys())
+dict_keys.sort()
+
 print()
-print("## keywords")
+print("# keywords")
 
 for key in dict_keys:
-	print("# "+key+"\t"+dictionary[key])
+	print("\n## "+key+"\t"+dictionary[key])
 	for index, row in df.iterrows():
 		Japanese = ""
 		if pd.notnull(row["BetterJP"]): #手動訳があれば利用
@@ -80,4 +82,4 @@ for key in dict_keys:
 			continue
 		elif row["English"].find(key) != -1: #キーワードを含む
 			if Japanese.find(dictionary[key]) == -1 : #日本語で使われていない
-				print("#"+str(row["N"])+"\n"+row["English"]+"\n"+Japanese)
+				print("### "+str(row["N"])+"\n"+row["English"]+"\n"+Japanese)
